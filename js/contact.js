5413d9f6556042d413572ba3cae9b18f12308fd7
root <root@DESKTOP-J8FJ0M1.localdomain>
2021-02-21 17:42:09 +0100

    last update

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,26 @@
 function import_data (){
-    const modalDisp = document.querySelector('#modal')
-  modalDisp.style.display = 'flex'
+
+
+  var name = document.getElementById("fname").value
+  var email = document.getElementById("fmail").value
+  var subj = document.getElementById("fsub").value
+  var message = document.getElementById("fmessage").value
+  document.getElementById("fullname").innerHTML = name
+  document.getElementById("email").innerHTML = email
+  document.getElementById("sub").innerHTML = subj
+  document.getElementById("message").innerHTML = message
+
+
+
+    const modalDisp = document.querySelector('#modal') 
+    modalDisp.style.display = 'flex'
   document.querySelector('#close').addEventListener('click', () => {
     document.querySelector('#modal').style.display = 'none'
+
+    
+
+
   })
+    
 }
+ 
